569b22a51b44a26232d83e5dec28d120f1196975
Avishkar Bachhav <avishkarbachhav45@gmail.com>
2025-11-20 21:13:41 +0530

    Added basic QR generation and Canvas rendering

--- app.js
+++ app.js
@@ -14,3 +14,30 @@ btn.addEventListener("click", () => {
 
     console.log("User input:", value);  
 });
+
+// generate QR and draw on canvas
+
+btn.addEventListener("click", () => {
+    const value = input.value.trim();
+
+    if (value === "") {
+        alert("Please enter some text or URL");
+        return;
+    }
+
+    const qr = new QRious({
+        value: value,
+        size: 200 //fixed size
+    });
+
+    // Render into canvas
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+
+    img.onload = () => {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+
+    img.src = qr.toDataURL();
+});
